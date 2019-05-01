c8b5382f87960c6f14a5e54d66d194b6253a8e2c
kodenaut <code3illa>
2019-05-01 21:09:42 +0300

    fixed duplicated DB insert

--- bookcrud/static/bookcrud/js/books.js
+++ bookcrud/static/bookcrud/js/books.js
@@ -1,42 +1,4 @@
 $(function () {
-$(".js-create-book").click(function () {
-    var btn=$(this);
-    $.ajax({
-        url: btn.attr("data-url"),
-        type: 'get',
-        dataType: 'json',
-        beforeSend: function () {
-        $("#modal-book").modal("show");
-        },
-        success: function (data) {
-        $("#modal-book .modal-content").html(data.html_form);
-        }
-        });
-    });
-});
-
-$("#modal-book").on("submit", ".js-book-create-form", function () {
-    var form = $(this);
-        $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function (data) {
-            if (data.form_is_valid) {
-                $("#book-table tbody").html(data.html_book_list); // <-- Replace the table body
-                $("#modal-book").modal("hide"); // <-- Close the modal
-            }
-            else {
-                 $("#modal-book .modal-content").html(data.html_form);
-            }
-            }
-        });
-    return false;
-});
-
-
-$(function () {
 /* Functions */
     var loadForm = function () {
     var btn = $(this);
@@ -66,11 +28,11 @@ var form = $(this);
     $("#modal-book").modal("hide");
     }
     else {
-$("#modal-book .modal-content").html(data.html_form);
-}
-}
-});
-return false;
+    $("#modal-book .modal-content").html(data.html_form);
+    }
+    }
+    });
+    return false;
 };
 /* Binding */
 // Create book
